4fab5fc5dab63d8c1c1ac633964c56d276174dba
lukaszzalewski <lukaszz@advisoril.com>
2018-07-23 11:45:49 -0400

    generate some test files

--- test/pipeline/scripts/create-some-files.sh
+++ test/pipeline/scripts/create-some-files.sh
@@ -8,3 +8,11 @@ echo 'Current Version'
 cat test-version/version
 
 echo 'Generating Files'
+
+echo 'file test' > generated-files/file1
+echo 'file test' > generated-files/file2
+echo 'file test' > generated-files/file3
+echo 'file test' > generated-files/file4
+echo 'file test' > generated-files/file5
+echo 'file test' > generated-files/file6
+echo 'file test' > generated-files/file7
